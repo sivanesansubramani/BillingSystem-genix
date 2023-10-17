934cac2807bc03010280f7181d34a75b153ddf5e
HariDurgesh <haridurgesh.thurkkaiappan@gmail.com>
2023-10-17 19:48:42 +0530

    DONE

--- BillingSystem/Models/Commonview.cs
+++ BillingSystem/Models/Commonview.cs
@@ -1,11 +1,16 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace BillingSystem.Models
 {
     public class Commonview
     {
+        [Required]
         public BillingAddressess BillingCreate { get; set; }
+        [Required]
 
         public ShippingAddress ShippingCreate { get; set; }
+        [Required]
 
         public AddProduct AddProduct { get; set; }
         public List<AddProduct> Cart {  get; set; }
